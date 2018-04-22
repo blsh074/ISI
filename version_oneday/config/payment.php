5272423145e210eeb6691d8c4db685b2effe1712
blsh074 <blsh074w@gmail.com>
2018-04-22 16:20:17 +0000

    testing1

--- version_oneday/config/payment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    return [
-      'uid' => env('PAYSAPI_UID'),
-      'token' => env('PAYSAPI_TOKEN'),
-      'notify_url' => env('PAYSAPI_NOTIFY_URL'),
-      'return_url' => env('PAYSAPI_RETURN_URL'),
-    ];
